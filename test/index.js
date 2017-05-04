47370bed6c7c9370dcab7df0c922d439efaec7ef
Paul Anderson <paul.anderson@uberops.com>
2017-05-03 20:55:53 -0400

    listen for created and updated events in tests

--- test/index.js
+++ test/index.js
@@ -6,11 +6,45 @@ const setupDB = require('./setupDB')
 before(setupDB.init)
 
 describe('upsert', () => {
-  it('inserts a new user when no matching one exists', () => {
-    return userModel.forge().upsert()
+  const aUser = {
+    email: 'one@test.com',
+    firstName: 'User',
+    lastName: 'One'
+  }
+  it('performs an insert when a table is freshly wiped', () => {
+    let created, updated
+    return userModel.forge(aUser)
+    .on('created', () => {
+      created = true
+    })
+    .on('updated', () => {
+      updated = true
+    })
+    .upsert()
     .then(user => {
-      should(user).be.ok()
-      user.get('email').should.be.ok()
+      should(created).be.ok()
+      should(updated).not.be.ok()
+      aUser.id = user.get('id')
+    })
+  })
+  it('performs an insert when keys are different', () => {
+    let created, updated
+    const anotherUser = {
+      email: 'two@test.com',
+      firstName: 'User',
+      lastName: 'Two'
+    }
+    return userModel.forge(anotherUser)
+    .on('created', () => {
+      created = true
+    })
+    .on('updated', () => {
+      updated = true
+    })
+    .upsert()
+    .then(user => {
+      should(created).be.ok()
+      should(updated).not.be.ok()
     })
   })
 })
